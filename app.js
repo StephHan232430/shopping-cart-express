3aa7148d31ab29b9024f2166e3f108d9175619ad
Steph <blackman914037@gmail.com>
2020-03-24 16:01:54 +0800

    Add post cart feature

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -7,6 +10,18 @@ const app = express()
 
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  session({
+    secret: 'ac',
+    name: 'ac',
+    cookie: { maxAge: 80000 },
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
